b44dea507c92b4b1084389fc70a3d18b499e0923
MichaelSaid <michael.said@incorta.com>
2022-12-19 21:09:37 +0200

    Add service

---
+++ src/main/java/com/task/countries/service/CountryService.java
@@ -0,0 +1,25 @@
+package com.task.countries.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.task.countries.model.Country;
+import com.task.countries.repository.CountryRepository;
+
+public class CountryService {
+    @Autowired
+    private CountryRepository countryRepository;
+
+    public List<Country> listAllCountry() {
+        return countryRepository.findAll();
+    }
+
+    public void saveCountry(Country country) {
+        countryRepository.save(country);
+    }
+
+    public Country getCountry(String code) {
+        return countryRepository.findById(code).orElse(null);
+    }
+}
