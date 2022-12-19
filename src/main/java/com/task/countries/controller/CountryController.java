3f5f4bf4ee84c45a5f7f2d3fc242f5c4c46f80f9
MichaelSaid <michael.said@incorta.com>
2022-12-19 21:34:00 +0200

    Add Pagination

--- src/main/java/com/task/countries/controller/CountryController.java
+++ src/main/java/com/task/countries/controller/CountryController.java
@@ -4,25 +4,29 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.task.countries.model.Country;
 import com.task.countries.service.CountryService;
 
 @RestController
-@RequestMapping("/Countries")
+@RequestMapping("/country")
 public class CountryController {
     @Autowired
     private CountryService countryService;
 
     @GetMapping("")
-    public List<Country> list() {
-        return countryService.listAllCountry();
+    public List<Country> list(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "50") int size) {
+        Pageable paging = PageRequest.of(page, size);
+        return countryService.listAllCountry(paging).getContent();
     }
 
     @GetMapping("/{name}")
@@ -34,4 +38,14 @@ public class CountryController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+
+//    @GetMapping("/sync")
+//    public ResponseEntity<Country> get(@PathVariable String name) {
+//        try {
+//            Country country = countryService.getCountry(name);
+//            return new ResponseEntity<>(country, HttpStatus.OK);
+//        } catch (NoSuchElementException e) {
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        }
+//    }
 }
--- src/main/java/com/task/countries/service/CountryService.java
+++ src/main/java/com/task/countries/service/CountryService.java
@@ -3,6 +3,8 @@ package com.task.countries.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.task.countries.model.Country;
@@ -20,8 +22,16 @@ public class CountryService {
         return countryRepository.findAll();
     }
 
+    public Page<Country> listAllCountry(Pageable pageable) {
+        return countryRepository.findAll(pageable);
+    }
+
     public void saveCountry(Country country) {
         countryRepository.save(country);
+    }
+
+    public void saveAll(List<Country> countries) {
+        countryRepository.saveAll(countries);
     }
 
     public Country getCountry(String name) {
